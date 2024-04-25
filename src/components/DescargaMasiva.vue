371533b0b1326f3fed8fd8655569bcce0f1ab83c
AnibalTiradoTEKIIO <anibal.tirado@tekiio.mx>
2024-04-25 17:56:24 -0600

    segundo commit

--- src/components/DescargaMasiva.vue
+++ src/components/DescargaMasiva.vue
@@ -3,32 +3,56 @@
 <template>
     <div>
       <h2>Descarga Masiva</h2>
-      <button @click="generarSolicitud">Generar Solicitud</button>
+          <!-- Formulario para capturar datos del usuario -->
+      <form @submit.prevent="enviarSolicitud">
+      <label for="rfcEmisor">RFC Emisor:</label>
+      <input type="text" id="rfcEmisor" v-model="rfcEmisor" required>
+      <label for="rfcReceptor">RFC Receptor:</label>
+      <input type="text" id="rfcReceptor" v-model="rfcReceptor" required>
+    <!-- Otros campos del formulario -->
+        <button type="submit">Enviar Solicitud</button>
       <button @click="consultarEstatus">Consultar Estatus</button>
+      </form>
     </div>
   </template>
   
   <script>
-  import axios from 'axios';
-  const apiUrl = 'https://descargamasiva.pade.mx/api/';
-  
-  export default {
-    methods: {
-      async generarSolicitud() {
-        try {
-          const response = await axios.post(apiUrl + 'solicitud/generar', {
-            // Aquí colocarías los parámetros requeridos para generar la solicitud
-            rfcEmisor: 'RFC del emisor',
-            rfcReceptor: 'RFC del receptor',
-            rfcSolicitante: rfcSolicitante,
+        import axios from 'axios';
+        const apiUrl = 'https://descargamasiva.pade.mx/api/';
 
+        export default {
+        data() {
+            return {
+            rfcEmisor: '',
+            rfcReceptor: '',
+            rfcSolicitante:'',
+            fechaInicio:'',
+            fechaFinal:'',
+            tipoSolicitud:'',
+            pfx:'',
+            password:'',
+            usuario:'',
+            passPade:'',
+            contrato:'',
+            // Otros datos necesarios para la solicitud
+            };
+        },
+        methods: {
+            async enviarSolicitud() {
+            try {
+                const response = await axios.post(apiUrl + 'solicitud/generar', {
+                rfcEmisor: this.rfcEmisor,
+                rfcReceptor: this.rfcReceptor,
+                // Otros datos de la solicitud
+                });
+                console.log('Solicitud enviada:', response.data);
+                // Aquí puedes manejar la respuesta según tus necesidades (mostrar mensajes, redirigir, etc.)
+            } catch (error) {
+                console.error('Error al enviar la solicitud:', error);
+                // Manejo de errores
+            }
+            },
 
-          });
-          console.log('Solicitud generada:', response.data);
-        } catch (error) {
-          console.error('Error al generar la solicitud:', error);
-        }
-      },
       async consultarEstatus() {
         try {
           const response = await axios.post(apiUrl + 'solicitud/estatus', {
